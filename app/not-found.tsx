0d302ff217f1cc177ddc847ac3dd3430d6dff7b1
gaevuha <gaevuha@gmail.com>
2025-08-09 12:55:38 +0300

    task-07 fix5

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -17,7 +17,7 @@ const NotFound = () => {
   return (
     <div>
       <h1 className={css.title}>404 - Page not found</h1>
-      <p className={`${css.description} ${css.notfound}`}>
+      <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
     </div>
